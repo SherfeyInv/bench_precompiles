bc9828fddd102f88053f143aea55e8722fdf5d65
Ronnie Lee Sherfey Jr <aysherintel@icloud.com>
2024-11-09 17:54:50 -0500

    .gitignore

--- run_blake2f_meter.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-RAYON_NUM_THREADS=4 cargo test --release -- --nocapture --ignored benchmark_blake2f_precompile
-
--- run_bn_mul_meter.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-RAYON_NUM_THREADS=1 cargo test --release -- --nocapture --ignored benchmark_bn_mul_precompile
-
